2f71e92ca502f083e1de6afcaf33dfbd72473fe7
hubert <kuchhubert@gmail.com>
2022-03-14 14:52:42 +0100

    Displaying Nav child's

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -1,22 +1,47 @@
 import React from 'react';
-export default function Nav() {
+
+type RouteEndpoint = {
+    url: string;
+    name: string;
+};
+
+const endpoints: RouteEndpoint[] = [
+    {
+        url: '/sticky-notes',
+        name: 'Sticky'
+    },
+    {
+        url: '/notes',
+        name: 'Notes'
+    },
+];
+
+export default function Nav({children}: { children?: JSX.Element }) {
     return (
         <nav className={'border-r-2 border-gray-300 h-full float-left'}>
             <div className="text-center font-light">
                 <span className={'text-sm'}>Notes</span>
             </div>
 
-            <div className={'grid mt-7'}>
-                <a
-                    className={'ml-5 mr-5 hover:text-gray-400'}
-                    href="/sticky-notes"
-                >
-                    Sticky
-                </a>
-                <a className={'ml-5 mr-5 hover:text-gray-400'} href="/notes">
-                    Notes
-                </a>
+            <div className={'grid mt-7 text-center border-b-2 border-gray-300'}>
+                {
+                    endpoints.map(({url, name}: RouteEndpoint, index: number) => {
+                        return <a
+                            className={`ml-5 mr-5 hover:text-gray-400 ${index === endpoints.length - 1 && 'mb-5'}`}
+                            href={url}
+                        >
+                            {name}
+                        </a>
+                    })
+                }
             </div>
+            {
+                /* REACT CHILDREN IF EXISTS */
+                children &&
+                <div className={'text-center mt-5'}>
+                    {children}
+                </div>
+            }
         </nav>
     );
 }
