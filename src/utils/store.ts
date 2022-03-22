364c9a352fe62e2507fab65e9a291d96da4a8503
hubert <kuchhubert@gmail.com>
2022-03-22 14:37:35 +0100

    Create inserting group action

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -1,6 +1,7 @@
-import {Action, createStore, Store} from "redux";
+import {createStore, Store} from "redux";
+import {Action, GroupT, StoreT} from "./types";
 
-enum MD_NOTES_ACTIONS {
+export enum MD_NOTES_ACTIONS {
     ADD_NOTE = 'MD_NOTES/ADD_NOTE',
     RENAME_NOTE = 'MD_NOTES/RENAME_NOTE',
 
@@ -10,14 +11,25 @@ enum MD_NOTES_ACTIONS {
 
 export let store: Store<Array<any>> = createStore(reducer);
 
-function reducer(state = [], action: Action) {
+function reducer(state: StoreT = {
+    groups: [], user: null
+}, action: Action) {
     switch (action.type) {
         case MD_NOTES_ACTIONS.ADD_NOTE:
-            return
+            return state
         case MD_NOTES_ACTIONS.RENAME_NOTE:
-            return
+            return state
         case MD_NOTES_ACTIONS.ADD_GROUP:
-            return
+            const {group, parentId} = action.payload;
+            group.id = 'd' + Math.random();
+
+            if (parentId) {
+                state.groups.find((group: GroupT) => group.id === parentId).innerGroups.push(group);
+            }
+
+            state.groups = [...state.groups, group];
+            console.log(state)
+            return {...state};
         case MD_NOTES_ACTIONS.RENAME_GROUP:
             return
         default:
